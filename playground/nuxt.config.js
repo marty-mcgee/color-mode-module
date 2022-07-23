aee092be821fd62aefbd4658d1484302d96c890e
Marty McGee <support@companyjuice.com>
2022-07-22 20:39:30 -0700

    v3.1.4.1 MM: nuxt@3.0.0-rc.6 + play/package.json{}

--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -4,14 +4,5 @@ import colorModeModule from '..'
 export default defineNuxtConfig({
   components: { global: true, dirs: ['~/components'] },
   css: ['~/assets/main.css'],
-  modules: [colorModeModule],
-  head: {
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      // { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
-      // { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
-      // { rel: 'manifest', href: '/site.webmanifest' }
-    ],
-  }
+  modules: [colorModeModule]
 })
